f8b2116a7f0542728079c4c2c435e328af084188
Vinicius <viniciusantos2105@gmail.com>
2024-07-26 07:54:30 -0300

    refactor:cleanup code

--- user-api/src/main/java/com/viniciusantos2105/userapi/dto/UserRequestDto.java
+++ user-api/src/main/java/com/viniciusantos2105/userapi/dto/UserRequestDto.java
@@ -16,9 +16,13 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class UserRequestDto implements Serializable {
-    @NotEmpty(message = "Nome não pode ser vazio") String userFullName;
+    @NotEmpty(message = "Nome não pode ser vazio")
+    String userFullName;
     @Email(message = "Email invalido")
-    @NotEmpty(message = "Email não pode ser vazio") String userEmail;
-    @NotEmpty(message = "Senha invalida") String userPassword;
-    @NotEmpty(message = "Tipo de usuario não pode ser vazio") String userType;
+    @NotEmpty(message = "Email não pode ser vazio")
+    String userEmail;
+    @NotEmpty(message = "Senha invalida")
+    String userPassword;
+    @NotEmpty(message = "Tipo de usuario não pode ser vazio")
+    String userType;
 }
